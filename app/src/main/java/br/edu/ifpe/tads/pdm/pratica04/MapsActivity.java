5a2b06df884ba6613be10ab4307705381b01cf9c
Wsanbey <welrysanbey@hotmail.com>
2022-05-25 20:56:36 -0300

    Parte 5: Trabalhando com a localização atual através do mapa

--- app/src/main/java/br/edu/ifpe/tads/pdm/pratica04/MapsActivity.java
+++ app/src/main/java/br/edu/ifpe/tads/pdm/pratica04/MapsActivity.java
@@ -23,7 +23,7 @@ import br.edu.ifpe.tads.pdm.pratica04.databinding.ActivityMapsBinding;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private static final Object FINE_LOCATION_REQUEST = false;
+    private static final Object FINE_LOCATION_REQUEST = 1;
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
     private boolean fine_location;
@@ -44,10 +44,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     //Esse método verifica se já temos a permissão que queremos
-    private void requestPermission() {int permissionCheck = ContextCompat.checkSelfPermission(this,
-            Manifest.permission.ACCESS_FINE_LOCATION);
+    private void requestPermission() {
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
         this.fine_location = (permissionCheck == PackageManager.PERMISSION_GRANTED);
+
         if (this.fine_location) return;
+
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, (Integer) FINE_LOCATION_REQUEST);
     }
@@ -58,6 +62,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         boolean granted = (grantResults.length > 0) &&
                 (grantResults[0] == PackageManager.PERMISSION_GRANTED);
         this.fine_location = (requestCode == (Integer) FINE_LOCATION_REQUEST) && granted;
+
+        if (mMap != null) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            mMap.setMyLocationEnabled(this.fine_location);
+        }
     }
 
     /**
@@ -75,15 +93,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng recife = new LatLng(-8.05, -34.9);
         LatLng caruaru = new LatLng(-8.27, -35.98);
         LatLng joaopessoa = new LatLng(-7.12, -34.84);
-        mMap.addMarker( new MarkerOptions().
+        mMap.addMarker(new MarkerOptions().
                 position(recife).
                 title("Recife").
                 icon(BitmapDescriptorFactory.defaultMarker(35)));
-        mMap.addMarker( new MarkerOptions().
+        mMap.addMarker(new MarkerOptions().
                 position(caruaru).
                 title("Caruaru").
                 icon(BitmapDescriptorFactory.defaultMarker(120)));
-        mMap.addMarker( new MarkerOptions().
+        mMap.addMarker(new MarkerOptions().
                 position(joaopessoa).
                 title("João Pessoa").
                 icon(BitmapDescriptorFactory.defaultMarker(230)));
@@ -93,6 +111,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             Toast.makeText(MapsActivity.this,
                     "Você clicou em " + marker.getTitle(),
                     Toast.LENGTH_SHORT).show();
+            //return;
+
+            mMap.setOnMyLocationClickListener(
+                    location -> Toast.makeText(MapsActivity.this,
+                            "Você está aqui!", Toast.LENGTH_SHORT).show());
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+
+            }
+            mMap.setMyLocationEnabled(this.fine_location);
+
             return false;
         });
 
@@ -102,6 +138,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 title("Adicionado em " + new Date()).
                 icon(BitmapDescriptorFactory.defaultMarker(0))));
 
+        mMap.setOnMyLocationButtonClickListener(
+                () -> {
+                    Toast.makeText(MapsActivity.this,
+                            "Indo para a sua localização.", Toast.LENGTH_SHORT).show();
+                    return false;
+                });
 
     }
 }
